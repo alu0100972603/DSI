abdd4bfadff6124c84aee6fe6cc7c85983c97403
Mireia Scholz <alu0100905144@ull.edu.es>
2018-03-24 20:31:00 +0000

    eliminar campo nombre de la validiación del formlario de inicio de sesión

--- javascripts/validar_index.js
+++ javascripts/validar_index.js
@@ -12,34 +12,32 @@ function validate_Email(email){
 
 function validate_form(){
 	
-	/* Obtenemos el valor del elemento con el ID especificado */
+	
+	alert("hola")
+	
+	//Obtenemos el valor del elemento con el ID especificado
 
-	var nombre = document.forms["Formulario"]["name"].value;
 	var email = document.forms["Formulario"]["mail"].value;
 	var contra = document.forms["Formulario"]["Password"].value;
-
 	
-	/* Comprobamos si los campos estan vacios o no cumplen las condiciones*/
+	//Comprobamos si los campos estan vacios o no cumplen las condiciones
 
-	if(nombre.length == 0){
-		alert("No se pueden dejar campos vacíos. Por favor introduzca un nombre.");
+	if(email.length == 0)
+	{
+		alert("No se pueden dejar campos vacíos. Por favor introduzca un email.");
 		return false;
 	}
-	else{
-		
-		if(email.length == 0){
-			alert("No se pueden dejar campos vacíos. Por favor introduzca un email.");
-			return false;
-		}
-		else{
-			if(validate_Email(email) == false){
-				alert("FORMATO INCORRECTO. POR FAVOR VUELVA A INTRODUCIR EL CORREO");
-				return false;
-			}
-			if(contra.length == 0){
-				alert("No se pueden dejar campos vacíos. Por favor introduzca una contraseña.");
-				return false;
-			}
-		}
+	else if(validate_Email(email) == false)
+	{
+		alert("Formato de e-mail incorrecto");
+		return false;
 	}
+	else if(contra.length == 0)
+	{
+		alert("No se pueden dejar campos vacíos. Por favor introduzca una contraseña.");
+		return false;
+	}
+	
+	return true
+	
 }
